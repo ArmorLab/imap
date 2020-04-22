1443717ff5ea78d53bf8ce8c4eb7c5f54ed8004f
Pawel Poloczek <pawel@poloczek.me>
2020-04-22 23:57:36 +0200

    Show trace in index

--- public/index.php
+++ public/index.php
@@ -6,33 +6,39 @@ include __DIR__ . '/../config/bootstrap.php';
 
 use ArmorLab\Driver\SocketImapDriver;
 
-echo '<html><head><meta charset="UTF-8" /><title>Mailbox</title></head><body>';
+try {
+    $imapDriver = new SocketImapDriver(
+        \constant('IMAP_HOST'), 
+        \constant('IMAP_PORT'),
+        \constant('IMAP_TIMEOUT')
+    );
 
-$imapDriver = new SocketImapDriver(
-    \constant('IMAP_HOST'), 
-    \constant('IMAP_PORT'),
-    \constant('IMAP_TIMEOUT')
-);
+    $imapDriver->login(\constant('IMAP_LOGIN'), \constant('IMAP_PASSWORD'));
 
-$imapDriver->login(\constant('IMAP_LOGIN'), \constant('IMAP_PASSWORD'));
+    $folders = $imapDriver->getAllFolders();
+    echo '<pre>';
+    var_dump($folders);
+    echo '</pre>';
 
-$folders = $imapDriver->getAllFolders();
-echo '<pre>';
-var_dump($folders);
-echo '</pre>';
+    $folders = $imapDriver->getActiveFolders();
+    echo '<pre>';
+    var_dump($folders);
+    echo '</pre>';
 
-$folders = $imapDriver->getActiveFolders();
-echo '<pre>';
-var_dump($folders);
-echo '</pre>';
+    $imapDriver->selectFolder("INBOX.spam   ");
 
-$imapDriver->selectFolder("INBOX.spam   ");
+    //'SINCE ' . date('j-M-Y', time() - 60 * 60 * 24 * 3)
+    $messages = $imapDriver->search('ALL');
 
-//'SINCE ' . date('j-M-Y', time() - 60 * 60 * 24 * 3)
-$messages = $imapDriver->search('ALL');
+    echo '<pre>';
+    var_dump($messages);
+    echo '</pre>';
+} catch (\Exception $exception) {
+    echo '<pre>';
+    var_dump($exception->getMessage());
+    echo '</pre>';
 
-echo '<pre>';
-var_dump($messages);
-echo '</pre>';
-
-echo '</body></html>';
+    echo '<pre>';
+    var_dump($exception->getTrace());
+    echo '</pre>';
+}
